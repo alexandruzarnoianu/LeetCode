f6f74d1eb2ffce4aadb2330be64a52ba798e6eff
alexandruzarnoianu <alex.zarnoianu@gmail.com>
2024-06-13 12:20:17 +0300

    Solved problem no.83 and no.61

---
+++ src/RemoveDuplicatesFromSortedList_83.java
@@ -0,0 +1,33 @@
+package src;
+
+
+public class RemoveDuplicatesFromSortedList_83 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+//    class Solution {
+//        public ListNode deleteDuplicates(ListNode head) {
+//            ListNode current = head;
+//            ListNode it = head;
+//            while (null != it) {
+//                while (current.val == it.val) {
+//                    if (it.next == null && it.val == current.val) {
+//                        current.next = null;
+//                        return head;
+//                    }
+//                    it = it.next;
+//                }
+//                current.next = it;
+//                current = it;
+//            }
+//            return head;
+//        }
+//    }
+}
