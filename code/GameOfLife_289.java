155c37b9c18a26fb614da46898e9ce2a972f2ee8
alexandruzarnoianu <alex.zarnoianu@gmail.com>
2024-07-03 07:13:54 +0300

    Updated solution for problem no.289. Solved in O(n*m) time complexity and O(1) space complexity.

--- code/GameOfLife_289.java
+++ code/GameOfLife_289.java
@@ -6,35 +6,32 @@ public class GameOfLife_289 {
     class Solution {
         public void gameOfLife(int[][] board) {
             int[][] directions = new int[][]{{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
-            ArrayList<ArrayList<Integer>> modifyIndex = new ArrayList<>();
             int m = board.length;
             int n = board[0].length;
             for (int i = 0; i < m; i++) {
-                ArrayList<Integer> modifyRow = new ArrayList<>();
                 for (int j = 0; j < n; j++) {
                     int neighbours = 0;
                     for (int[] direction : directions) {
                         int row = i + direction[0];
                         int col = j + direction[1];
-                        if (row < 0 || row >= m || col < 0 || col >= n || board[row][col] == 0) {
+                        if (row < 0 || row >= m || col < 0 || col >= n
+                                || board[row][col] == 0 || board[row][col] == 2) {
                             continue;
                         }
                         neighbours++;
                     }
                     if (board[i][j] == 1 && (neighbours < 2 || neighbours > 3)) {
-                        modifyRow.add(j);
+                        board[i][j] = 10;
                     } else if (board[i][j] == 0 && neighbours == 3) {
-                        modifyRow.add(j);
+                        board[i][j] = 2;
                     }
                 }
-                modifyIndex.add(modifyRow);
             }
-            for (int i = 0; i < modifyIndex.size(); i++) {
-                ArrayList<Integer> modifyRow = modifyIndex.get(i);
-                for (int j : modifyRow) {
-                    if (board[i][j] == 1) {
+            for (int i = 0; i < board.length; i++) {
+                for (int j =0; j < board[0].length; j++) {
+                    if (board[i][j] == 10) {
                         board[i][j] = 0;
-                    } else if (board[i][j] == 0) {
+                    } else if (board[i][j] == 2) {
                         board[i][j] = 1;
                     }
                 }
